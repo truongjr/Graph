3c7f416359084501a0de13de349865940f786c77
truongjr <nguyentruongjr@gmail.com>
2021-04-09 12:29:56 +0700

    cap nhat

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@
 using namespace std;
 //VE MAN HINH
 void createScreenWelcome(){
-	initwindow(1009, 713);
+	initwindow(1209, 813);
 	settextstyle(1, HORIZ_DIR, 1);
 	outtextxy(100, 100, "CHAO MUNG DEN VOI CHUONG TRINH DEMO DO THI");
 	int mx=getmaxx()/2; int my=getmaxy()/2;
@@ -41,13 +41,13 @@ void createScreenWelcome(){
 	floodfill(mx-20,my+30,YELLOW);
 	floodfill(mx-50,my-20,YELLOW);
 	floodfill(mx+50,my-20,YELLOW);
-	Sleep(3500);
+	Sleep(3000);
 	closegraph();
 }
 void CreateScreen(int maxx, int maxy){
-	char Menu[9][5] = {"DFS", "BFS", "X->Y", "TPLT", "HMT", "EUL", "DTR", "DTH", "CCA"};
-	char Toolbar[8][6] = {"New", "Open", "Save", "AddV", "AddE", "Move", "RName", "Del"};
-	initwindow(1009, 713);
+	char Menu[9][10] = {"DFS", "BFS", "X->Y", "TPLT", "Hamilton", "Euler", "Dinh tru", "Dinh that", "Canh cau"};
+	char Toolbar[8][10] = {"New", "Open", "Save", "AddVertex", "AddEdge", "Move", "Rename", "Delete"};
+	initwindow(1209, 813);
 	setbkcolor(15);
 	setcolor(BLUE);
 	setlinestyle(0, 0, 10);
@@ -58,49 +58,56 @@ void CreateScreen(int maxx, int maxy){
 	settextstyle(4, HORIZ_DIR, 5);
 	//ve thanh cong cu
 	for(int i=1; i<8; i++){
-		rectangle(10 + i*123, 10, 10 + (i-1)*123, 52);
-		settextstyle(4, HORIZ_DIR, 1);
-		outtextxy(10 + (i-1)*123 + 10, 10 + 10, Toolbar[i-1]);
+		rectangle(10 + i*(maxx/8-5), 10, 10 + (i-1)*(maxx/8-5), 52);
+		settextstyle(3, HORIZ_DIR, 2);
+		outtextxy(10 + (i-1)*(maxx/8 - 5) + 10, 10 + 10, Toolbar[i-1]);
 		if(i == 7){
-			outtextxy(10 + (i)*123 + 10, 10 + 10, Toolbar[i]);
+			outtextxy(10 + (i)*(maxx/8-5) + 10, 10 + 10, Toolbar[i]);
 		}
 	}
 	//ve khung thanh cong cu
 	rectangle(10, 10, maxx - 10, 52);
 	
 	//ve khung menu
-	rectangle(10, 58, 326, 366);
+	rectangle(10, 58, maxx/3 + 2, maxy/2-5);
+	rectangle(10, 58, maxx/3 + 2, 95);
+	outtextxy(150, 65, "ALGORITHM");
 	//ve bang menu. co su support cua Hung
 	int c = 0;// bien c la bien lay vi tri cua ten menu trong mang Menu
 	for(int i = 1; i <= 3; i++){
 		for(int j = 1; j <= 3; j++){
-			rectangle(10 + j*100 + (j)*4, 62 + (i-1)*76, 10 + (j-1)*100 + j*4, 72+62 + (i-1)*76);
+			rectangle(10 + j*125 + (j)*4, 95 + (i-1)*69 + (i)*4, 10 + (j-1)*125 + j*4, 69 + 95 + (i-1)*69 + i*4);
+			//123 la chieu dai cua tung o trong menu; 10, 95 lan luot la vi tri bat dau cua x, y; 65 la do rong cua o
 			//setbkcolor(14);
-			outtextxy(10 + j*100 - 80 + j*4, 62 + (i-1)*76 + 25, Menu[c]);
+			outtextxy(10 + (j-1)*123 + j*4 + 20, 80 + (i-1)*76 + 35, Menu[c]);
 			c++;
 		}
 	}
 	//ve o Topo Sort
-	rectangle(14, 290, 322, 362);
+	rectangle(15, 318, maxx/3 - 2, maxy/2-5-4);
 	//ghi chu Topo Sort
-	setfillstyle(1, 7); bar(14, 290, 322, 362);
-	setbkcolor(7);
-	outtextxy(100, 315, "Topo Sort");
-	
-	
-	//ve khung ma tran don vi
-	rectangle(10, 372, 326, maxy - 10);
+	//setfillstyle(1, 7); bar(10, 320, 322, 362);
+	//setbkcolor(7);
+	outtextxy(155, 340, "Topo Sort");
 	
 	//ve khung huong dan
-	rectangle(332, 550, maxx - 10, maxy -10);
+	rectangle(maxx/3 + 9, 601, maxx - 10, maxy -10);
 	
 	//ve khung xu ly
-	rectangle(332, 58, maxx - 10, 544);
+	rectangle(maxx/3 + 9, 58, maxx - 10, 595);
 	
-	///////////////////////////////////////////////////
+	//ve khung ma tran don vi
+	rectangle(10, maxy/2, maxx/3 + 2, maxy - 10);
 	//ghi chu MA TRAN TRONG SO
-	setbkcolor(WHITE);
-	outtextxy(18, 382, "MA TRAN TRONG SO");
+	rectangle(10, maxy/2, maxx/3 + 2, maxy/2 + 40);
+	//setbkcolor(WHITE);
+	outtextxy(100, 410, "MA TRAN TRONG SO");
+	for(int j=0; j<14; j++){
+		for(int i=0; i<14; i++){
+			rectangle(10 + i*28, maxy/2+40 + j*25, 10 + i*28 + 28, maxy/2 + 40 + 25 + j*25);
+		}
+	}
+		
 	
 }
 //TAO NODE
@@ -113,11 +120,12 @@ void CreateNode(int x, int y, char name[], int color){
 }
 //
 
+
 int main(){
-	createScreenWelcome();
-	CreateScreen(1000, 700);
-	char node1[] = "00";
-	CreateNode(500, 500, node1, BLACK); 
+	//createScreenWelcome();
+	CreateScreen(1200, 800);
+//	char node1[] = "00";
+//	CreateNode(500, 500, node1, BLACK); 
 	while(!kbhit());
 	return 0;
 }
