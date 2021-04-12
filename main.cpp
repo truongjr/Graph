b6f430fa3f5c820238db0fef55c8bdbbf59e912f
truongjr <nguyentruongjr@gmail.com>
2021-04-12 22:54:36 +0700

    cn

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 #include<string>
 #include<windows.h>
+#include<algorithm>
 #define BLACK 0
 #define BLUE 1 
 #define GREEN 2 
@@ -40,6 +41,7 @@ void SubEffectFile(int x1, int y1, int x2, int y2, char a[], int color);
 void SubEffectToolbar(int x1, int y1, int x2, int y2, char a[], int color);
 void SubEffectMenu(int x1, int y1, int x2, int y2, char a[], int color);
 void Effect(bool flag);
+void drawcurved1(int x1, int y1, int x2, int y2, int color);
 int main(){
 	Node *node[15];
 	int numberNode = 0;
@@ -562,3 +564,51 @@ void Effect(bool flag){
 		} 
 	}
 }
+void drawcurved1(int x1, int y1, int x2, int y2, int color) {
+	setcolor(color);
+	double s90 = sin(90 * M_PI / 180.0);    
+  	double c90 = cos(90 * M_PI / 180.0);
+  	// quay doan thang mot goc 90 do
+  	double midx = (x1 + x2) * 1.0 / 2, midy = (y1 + y2) * 1.0 / 2;
+  	double xI =	c90 * (x2 - midx) - s90 * (y2 - midy) + midx; 
+  	double yI = s90 * (x2 - midx) + c90 * (y2 - midy) + midy;
+  	// tinh toa do cua cac giao diem cua duong tron tam I voi duong tron node 
+  	double r1 = sqrt(pow(xI - x1, 2) + pow(yI - y1, 2));
+  	double d = r1;
+  	double r2 = 25;
+  	double a = (r1 * r1 - r2 * r2 + d * d) / (2 * d);
+  	double h = sqrt(r1 * r1 - a * a);
+  	double tempx1 = xI + a * (x1 - xI) / d;
+  	double tempy1 = yI + a * (y1 - yI) / d;
+  	double tempx2 = xI + a * (x2 - xI) / d;
+  	double tempy2 = yI + a * (y2 - yI) / d;
+  	double x3 = tempx1 - h * (y1 - yI) / d;
+  	double y3 = tempy1 + h * (x1 - xI) / d;
+  	double x4 = tempx2 + h * (y2 - yI) / d;
+	double y4 = tempy2 - h * (x2 - xI) / d;
+	double param1 = (x3 - xI) * 1.0 / r1;
+	double param2 = (x4 - xI) * 1.0 / r1;
+	// tinh goc quet
+	double angle1 = acos(param1) * 180.0 / M_PI;
+	double angle2 = acos(param2) * 180.0 / M_PI;
+	double startAngle, endAngle;
+	int cnt;
+	if (x3 >= xI && y3 <= yI && x4 >= xI && y4 <= yI)
+		startAngle = -360 + angle2, endAngle = -360 + angle1, cnt = 1;
+	else if (x3 >= xI && y3 <= yI && x4 >= xI && y4 > yI) 
+		startAngle = -angle2, endAngle = -360 + angle1, cnt = 2;
+	else if (x3 >= xI && y3 > yI && x4 >= xI && y4 > yI)
+		startAngle = -angle2, endAngle = -angle1, cnt = 3;
+	else if (x3 >= xI && y3 > yI && x4 < xI && y4 > yI)
+		startAngle = -angle2, endAngle = -angle1, cnt = 4;
+	else if (x3 < xI && y3 > yI && x4 < xI && y4 > yI) 
+		startAngle = -angle2, endAngle = -angle1, cnt = 5;
+	else if (x3 < xI && y3 > yI && x4 < xI && y4 <= yI) 
+		startAngle = -360 + angle2, endAngle = -angle1, cnt = 6;
+	else if (x3 < xI && y3 <= yI && x4 < xI && y4 <= yI)
+		startAngle = -360 + angle2, endAngle = -360 + angle1, cnt = 7;
+	else if (x3 < xI && y3 <= yI && x4 >= xI && y4 <= yI)
+		startAngle = -360 + angle2, endAngle = -360 + angle1, cnt = 8;
+	arc(xI, yI, startAngle, endAngle, r1);
+}
+
